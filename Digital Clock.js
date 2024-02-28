50794253e8601f4b23d911c39393b1b8387b3b5d
Mahfuj Islam <mahafuzislam7417@gmail.com>
2024-02-28 20:35:51 +0600

    First commit

---
+++ Digital Clock.js
@@ -0,0 +1,14 @@
+
+
+setInterval(() => {
+    const currentTime = new Date();
+
+hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    
+},100);
+
+
+
+
